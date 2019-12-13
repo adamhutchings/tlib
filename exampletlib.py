622c66034f9e82ca61beff9ca003f3248446c2a0
Adam Hutchings <adamhutchings@Adams-MacBook-Pro.local>
2019-12-12 22:54:35 -0800

    Update the test to reflect recent additions

--- exampletlib.py
+++ exampletlib.py
@@ -7,12 +7,22 @@ window = tlib.DefaultWindow('Example')
 # Creating a GameObject (color, speed, coords, pixel size, shape)
 example = tlib.GameObject('#21a6f6', 10, -100, 0, 40, 40, 'square')
 example2 = tlib.GameObject('#dd2323', 10, 100, 0, 40, 40, 'circle')
+example3 = tlib.StaticBox('#194572', 20, 200, 60, 20)
 
 # Movement binds on a window, with keybinds as up-down-left-right
 example.simple_binds(['w', 's', 'a', 'd'], window)
-example2.simple_binds(['h','k','j','l'], window)
 
+# These are more keybinds
+example2.simple_binds(['u','j','h','k'], window)
+
+# This probably could be shortened, but not now.
+window.run()
+
+ti.sleep(1)
+example3.align_left(0)
+
+# Will clean this kind of loop up later.
 while True:
 	window.run()
-
-window.screen.mainloop()
+	example.move(example.unit_vec(example2))
+	ti.sleep(0.05)
